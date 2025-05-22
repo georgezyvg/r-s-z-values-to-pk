efb7dcab34c8e3ded44e970e3fdba87bfb48b3df
georgezyvg <georgeputta355@gmail.com>
2025-05-22 10:00:55 +0100

    Update rsz2pk.py

--- rsz2pk.py
+++ rsz2pk.py
@@ -35,7 +35,7 @@ def recover_private_key(bitcoin_address):
                     signatures[r_value] = (s_value, z_value)
 
 def process_addresses(file_path):
-    with open(file id, 'r') as file:
+    with open(fileid, 'r') as file:
         addresses = file.read().splitlines()
     
     for address in addresses:
